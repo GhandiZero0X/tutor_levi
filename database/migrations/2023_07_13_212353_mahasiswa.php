5178117b642ef99f12b8d84da3f3516695f494b6
levinanj <anjanilevina@gmail.com>
2023-07-14 04:31:27 +0700

    hapus migrasi
    hapus template

--- database/migrations/2023_07_13_212353_mahasiswa.php
+++ database/migrations/2023_07_13_212353_mahasiswa.php
@@ -4,21 +4,33 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class mahasiswa extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        //
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('NAMA');
+            $table->string('NIS');
+            $table->string('ALAMAT');
+            $table->string('EMAIL');
+            $table->string('PASSWORD');
+            $table->timestamps();
+        });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        //
+        Schema::dropIfExists('mahasiswa');
     }
-};
+}
